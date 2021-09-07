81d6e7f0c31d81f7881c72bde940ebe1758f0e5b
Bers <mbers.pro@gmail.com>
2021-09-07 23:05:34 +0300

    Redone

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
+const debounce = require('lodash.debounce');
 import './style.css';
-import handleNode from './templates/templates.hbs'
-import singleCountryTemlate from "./templates/singleCountryTemlate.hbs"
-import {alert} from '@pnotify/core';
+import handleNode from './templates/templates.hbs';
+import singleCountryTemlate from './templates/singleCountryTemlate.hbs';
+import { alert } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 
-
 const refs = {
-    inputNode: document.querySelector('#inputNode'),
-    ulNode: document.querySelector('#ulNode'),
-}
-const fetchUrl = (arg1) => {
-    const baseUrl = "https://restcountries.eu/rest/v2/name/"
-    fetch(baseUrl + arg1).then(item => item.json()).then(item => {
-        const test = handleNode(item);
-        const test2 = singleCountryTemlate(item);
-        console.log(item)
-        
-        if(item.length === 1){
-            refs.ulNode.innerHTML = test2;
-        }else if(item.length < 10 && item.length >= 2){
-            refs.ulNode.innerHTML = test;
-        }else{
-            alert({
-                text: 'Notice me, senpai!'
-              });
-        }
-        
-    })
-    
-}
+  inputNode: document.querySelector('#inputNode'),
+  ulNode: document.querySelector('#ulNode'),
+};
+const fetchUrl = arg1 => {
+  const baseUrl = 'https://restcountries.eu/rest/v2/name/';
+  fetch(baseUrl + arg1)
+    .then(item => item.json())
+    .then(item => {
+      const test = handleNode(item);
+      const test2 = singleCountryTemlate(item);
+      console.log(item);
+
+      if (item.length === 1) {
+        refs.ulNode.innerHTML = test2;
+      } else if (item.length < 10 && item.length >= 2) {
+        refs.ulNode.innerHTML = test;
+      } else {
+        alert({
+            text: 'Too many matches found. Please enter a more specific query!',
+            delay: 500
+        });
+      }
+    });
+};
 const onInput = () => {
-    const inputVal = refs.inputNode.value
-    fetchUrl(inputVal)
-}
-refs.inputNode.addEventListener("input", onInput)
+  const inputVal = refs.inputNode.value;
+  fetchUrl(inputVal);
+};
+refs.inputNode.addEventListener('input', debounce(onInput, 500));
